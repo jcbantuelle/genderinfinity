d62b159c9e020cf8bd14fcd107b9632b9b9f54da
Justin Bantuelle <justin@mudbugmedia.com>
2016-09-14 12:45:37 -0500

    Update Production ActionMailer Config Domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'genderinfinity.org',
     :enable_starttls_auto => true
   }
 end

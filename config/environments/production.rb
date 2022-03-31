f5165d615be31c803c307e6b327381bbdf09f686
Justin Bantuelle <justin@mudbugmedia.com>
2022-03-30 22:07:37 -0500

    Add ActionMailer Default Url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => "resource.genderinfinity.org" }
+
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
